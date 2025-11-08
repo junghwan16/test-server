7d799312db4977c5dc064169fae6e3409c7f2356
junghwan16 <chotnt741@gmail.com>
2025-11-09 00:29:16 +0900

    add log

--- main.go
+++ main.go
@@ -2,10 +2,9 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
+	"log"
 	"net/http"
 	"sync/atomic"
-	"time"
 )
 
 var (
@@ -19,16 +18,7 @@ func main() {
 	http.HandleFunc("/ready", handleReady)
 	http.HandleFunc("/live", handleLive)
 
-	// Hello API
-	http.HandleFunc("/hello", handleHello)
-
-	// 응용 1: startupProbe 실패 유발
-	http.HandleFunc("/startup-fail-on", func(w http.ResponseWriter, r *http.Request) {
-		atomic.StoreInt32(&isStartupOk, 0)
-		fmt.Fprintln(w, "Set to fail startupProbe. The pod will restart indefinitely if a startupProbe is configured.")
-	})
-
-	// 응용 2: 부하 ON/OFF 및 관련 API
+	// 부하 ON/OFF 및 관련 API
 	http.HandleFunc("/server-load-on", handleServerLoadOn)
 	http.HandleFunc("/server-load-off", handleServerLoadOff)
 
@@ -37,27 +27,10 @@ func main() {
 	http.ListenAndServe(addr, nil)
 }
 
-func handleHello(w http.ResponseWriter, r *http.Request) {
-	greetings := []string{
-		"Hello, World!",
-		"Live long and prosper.",
-		"May the Force be with you.",
-		"So long, and thanks for all the fish.",
-		"Never give up, never surrender!",
-		"Go forth, and multiply.",
-		"To infinity, and beyond!",
-		"Excelsior!",
-	}
-
-	rand.Seed(time.Now().UnixNano())
-	greeting := greetings[rand.Intn(len(greetings))]
-
-	w.WriteHeader(http.StatusOK)
-	fmt.Fprintln(w, greeting)
-}
-
 // 앱 초기화 체크
 func handleStartup(w http.ResponseWriter, r *http.Request) {
+	log.Println("startup probe called")
+
 	// startupProbe 실패 강제 (응용 1)
 	if atomic.LoadInt32(&isStartupOk) == 0 {
 		http.Error(w, "startup failing (forced by /startup-fail-on)", http.StatusInternalServerError)
@@ -70,6 +43,8 @@ func handleStartup(w http.ResponseWriter, r *http.Request) {
 
 // DB 연결, 캐시 로드 등 준비 상태 체크
 func handleReady(w http.ResponseWriter, r *http.Request) {
+	log.Println("ready probe called")
+
 	if atomic.LoadInt32(&isReady) == 0 {
 		http.Error(w, "ready=false", http.StatusServiceUnavailable)
 		return
@@ -80,6 +55,8 @@ func handleReady(w http.ResponseWriter, r *http.Request) {
 
 // 단순 핑 응답 (일반적인 /healthz 역할)
 func handleLive(w http.ResponseWriter, r *http.Request) {
+	log.Println("live probe called")
+
 	if atomic.LoadInt32(&isLive) == 0 {
 		http.Error(w, "live=false", http.StatusInternalServerError)
 		return
